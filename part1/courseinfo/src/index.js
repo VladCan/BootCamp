3bc1b7501e3bde17ab4cf7e486626af3ed523269
VladCan <vladymir.canchari@gmail.com>
2022-06-22 00:02:51 -0500

    Ejercicio 1.4 objetos en un array

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -30,27 +30,27 @@ const Total = (props) =>{
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 /*Lista entre []*/
 /* Suma  como una suma*/
   return (
     <div>
       <Header course={course}/>    
       
-      <Content part={[part1.name,part2.name,part3.name]} exercises={[part1.exercises,part2.exercises,part3.exercises]}/>
-
-      <Total suma={part1.exercises+part2.exercises+part3.exercises}/>
+  
     </div>
   )
 }
