b6644f22f1de7787298f76155f1f3526b70d693d
VladCan <vladymir.canchari@gmail.com>
2022-06-21 23:51:58 -0500

    Ejercicio 1.3 modificacion del const App, hay que refactorisar otra vez

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom'
 /*const Header = ({course}) =>   <h1>{course}</h1>*/
 
 const Header = (props) =>{
+  console.log(props)
   return   <h1>{props.course}</h1>
 }
 const Part = (props) =>{
@@ -29,19 +30,24 @@ const Total = (props) =>{
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 /*Lista entre []*/
 /* Suma  como una suma*/
   return (
     <div>
       <Header course={course}/>    
-      <Content part={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
-      <Total suma={exercises1+exercises2+exercises3}/>
+
     </div>
   )
 }
